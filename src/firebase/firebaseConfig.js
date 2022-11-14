ce3255ea305f4022dc887711ea23a9febe91b55d
aim4dabush <edarasato@gmail.com>
2022-11-14 15:28:37 -0800

    installed firebase and setup firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA-759kH8I82n8oqmCPWSHlS_OwtE-pKc",
+  authDomain: "e-commerce-30411.firebaseapp.com",
+  projectId: "e-commerce-30411",
+  storageBucket: "e-commerce-30411.appspot.com",
+  messagingSenderId: "413379540836",
+  appId: "1:413379540836:web:ec0ff0cc35a1ed7ae86c71",
+};
+
+const app = initializeApp(firebaseConfig);
