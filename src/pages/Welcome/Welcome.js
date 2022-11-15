722cc72cd9e8a7526efb8b019c265e96755a6852
aim4dabush <edarasato@gmail.com>
2022-11-15 09:35:53 -0800

    finished Welcome Page

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,7 +1,33 @@
-import React from "react";
+//components
+import { GiShop } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
+//styles
+import styles from "./Welcome.module.scss";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.card}>
+        <GiShop className={styles.icon} />
+        <h2 className={styles.title}>Don't Shop Believin</h2>
+        <p className={styles.message}>
+          Don't be strangers waitin up and down the boulevard or shadows
+          searchin in the night. Live to find the emotion somewhere in the
+          night. Before you{" "}
+          <span className={styles.brand}>Don't Shop Believin</span>, please{" "}
+          <NavLink className={styles.link} to="/signup">
+            sign-up
+          </NavLink>{" "}
+          for your free account. If you already have an account you can login{" "}
+          <NavLink className={styles.link} to="/login">
+            here
+          </NavLink>
+          .
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
