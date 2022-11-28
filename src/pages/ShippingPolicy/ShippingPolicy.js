5adb20d909e811a2987729e8a9432d1730104532
aim4dabush <edarasato@gmail.com>
2022-11-28 11:47:22 -0800

    finished shipping policy page

--- src/pages/ShippingPolicy/ShippingPolicy.js
+++ src/pages/ShippingPolicy/ShippingPolicy.js
@@ -1,7 +1,68 @@
-import React from "react";
+//styles
+import styles from "./ShippingPolicy.module.scss";
 
 const ShippingPolicy = () => {
-  return <div>ShippingPolicy</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Shipping Policy</h1>
+      <ul>
+        <li>
+          <h3>When we ship?</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+            explicabo, optio dolorem exercitationem ex deserunt atque nostrum
+            libero officia deleniti reprehenderit fugiat. Sed nesciunt nihil
+            adipisci nobis suscipit. Repudiandae, sit?
+          </p>
+        </li>
+        <li>
+          <h3>Where we ship to?</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            magni temporibus aliquid hic, eos animi id odit nesciunt quia quo
+            dolorem quos doloribus ab similique deserunt exercitationem, iure
+            eveniet aperiam.
+          </p>
+        </li>
+        <li>
+          <h3>How we ship</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vel
+            odio minima esse quia natus officia, voluptatum ducimus alias,
+            inventore consectetur incidunt. Ex rerum possimus aliquam expedita
+            esse iure sequi?
+          </p>
+        </li>
+        <li>
+          <h3>Rates of shipping?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            quo voluptatem ratione, similique iusto nisi voluptates debitis
+            perferendis unde aliquid. Officia cumque ea velit blanditiis
+            obcaecati in porro perspiciatis beatae!
+          </p>
+        </li>
+        <li>
+          <h3>How to track your order?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, mollitia sint! Placeat, dolores iusto! Dignissimos,
+            temporibus consequuntur repellendus veritatis enim illo quisquam
+            eaque fugiat? Itaque consequuntur consectetur deserunt dolore esse.
+          </p>
+        </li>
+        <li>
+          <h3>Shipped to wrong address?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+            ducimus laudantium facere consequatur sapiente dolore reiciendis
+            impedit iusto ea similique, facilis magni illo assumenda pariatur,
+            corporis incidunt? Eum, nulla molestias?
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ShippingPolicy;
