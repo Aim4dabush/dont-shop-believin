13b2fcaef39c56ece3e96aa9e1ef800e8f9b3d71
aim4dabush <edarasato@gmail.com>
2022-12-13 11:36:57 -0800

    nav bar responsive

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
+
 //components
 import HeaderBrand from "./HeaderBrand/HeaderBrand";
 import LinkList from "./LinkList/LinkList";
+import { FaBars } from "react-icons/fa";
 
 //styles
 import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const showLinksHandler = () => {
+    setShowLinks((prev) => (prev = !prev));
+  };
   return (
     <div className={styles.container}>
       <HeaderBrand />
-      <LinkList />
+      {showLinks && <LinkList />}
+      <FaBars className={styles.bars} onClick={showLinksHandler} />
     </div>
   );
 };
